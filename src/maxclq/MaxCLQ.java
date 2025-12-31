16c56893fc3d4cc66db02c33ceb3ae2bf6ef8df3
Daban4 <149517465+Daban4@users.noreply.github.com>
2025-12-31 10:19:23 +0100

    _atmost cambiado

--- src/maxclq/MaxCLQ.java
+++ src/maxclq/MaxCLQ.java
@@ -1,4 +1,5 @@
 package maxclq;
+
 /**
  *
  * @author MAZ
@@ -20,193 +21,210 @@ import java.util.Set;
 import ugraph.Node;
 import ugraph.UGraph;
 //
+
 public final class MaxCLQ implements MaxCliqueSolver {
 
-  static {
-    System.loadLibrary("jniortools");
-  }
-  
-  private final BruteForceMaxCliqueSolver pereborSolver = new BruteForceMaxCliqueSolver();
-  
-  @Override
-  public Set<Node> solve (final UGraph G) {
-        
-    final Set<Node> C = _solve(G, new HashSet<>(), 0); //el hashet es el clike en construccion 
-    
-    if (!verify(G, C))
-      throw new IllegalArgumentException("Clique mal formado");
-    
-    return C;
-    
-  }
-  
-  private Set<Node> _solve (final UGraph G, Set<Node> C, int LB) { 
+    static {
+        System.loadLibrary("jniortools");
+    }
+
+    private final BruteForceMaxCliqueSolver pereborSolver = new BruteForceMaxCliqueSolver();
+
+    @Override
+    public Set<Node> solve(final UGraph G) {
+
+        final Set<Node> C = _solve(G, new HashSet<>(), 0); //el hashet es el clike en construccion 
+
+        if (!verify(G, C)) {
+            throw new IllegalArgumentException("Clique mal formado");
+        }
+
+        return C;
+
+    }
+
+    private Set<Node> _solve(final UGraph G, Set<Node> C, int LB) {
 //pasamos el grafo que nos va quedando, no siempre es el original 
 // el set es el clique en construccion y LB es el mejor tamaño hasta ahora
 
-    // Si el grafo es vacÃ­o se devuelve el clique en construcciÃ³n.
-    if (G.nodes() == 0)
-      return C;
-      
-    // Si el grafo tiene un Ãºnico nodo o no tiene aristas, se devuelve
-    // el nodo de grado mÃ¡ximo.
-    if ((G.nodes() == 1) || (G.degree() == 0)){    
-      C.add(G.getMaxsDegreeNode());
-      return C;
+        // Si el grafo es vacÃ­o se devuelve el clique en construcciÃ³n.
+        if (G.nodes() == 0) {
+            return C;
+        }
+
+        // Si el grafo tiene un Ãºnico nodo o no tiene aristas, se devuelve
+        // el nodo de grado mÃ¡ximo.
+        if ((G.nodes() == 1) || (G.degree() == 0)) {
+            C.add(G.getMaxsDegreeNode());
+            return C;
+        }
+
+        // Si la instancia es pequeÃ±a respecto al grafo original,
+        // se resuelve mediante fruerza bruta.
+        if (10 * G.nodes() <= G.support().nodes()) {
+            C.addAll(pereborSolver.solve(G)); //fuerza bruta de la semana anaterior
+            return C;
+        }
+
+        final int k = LB - C.size();
+        final int UB = overstimation(G, k) + C.size();
+
+        // Si la estimaciÃ³n informa que en el grafo G no hay
+        // un clique de tamaÃ±o mayor que LB, se corta esta rama
+        // de la exploraciÃ³n recursiva.
+        if (LB >= UB) {
+            return new HashSet<>();
+        }
+
+        final Node v = G.getMaxDegreeNode();
+        final UGraph Gv = G.subgraphInducedBy(v);
+        final Set<Node> _C = new HashSet<>(C); //vecinos de V sin V creo que es esto 
+        _C.add(v);
+
+        final Set<Node> C1 = _solve(Gv, _C, LB);
+        if (C1.size() > LB) {
+            LB = C1.size();
+        }
+
+        final UGraph Gm = G.subgraphInducedMinus(v, k);
+        final Set<Node> C2 = _solve(Gm, C, LB);
+
+        return (C1.size() > C2.size()) ? C1 : C2;
+
     }
-    
-    // Si la instancia es pequeÃ±a respecto al grafo original,
-    // se resuelve mediante fruerza bruta.
-    if (10 * G.nodes() <= G.support().nodes()) {
-      C.addAll(pereborSolver.solve(G)); //fuerza bruta de la semana anaterior
-      return C;
+
+    private int overstimation(final UGraph G, final int k) {
+
+        // Se obtiene una particiÃ³n del grafo.
+        final List<Set<Node>> P = UGraphUtilities.partition(G); //implementar en este metodo estatico el algoritmo voraz
+        //P es una particion
+
+        // Sil particiÃ³n no tiene suficientes subconjuntos, no se resuelve.
+        if (P.size() < k) {
+            return 0;
+        }
+
+        return solveMaxSATModel(G, k, P); //intentamos mejorar la sobreestimacion
+
     }
 
-    final int k = LB - C.size();
-    final int UB = overstimation(G, k) + C.size();
+    private int solveMaxSATModel(final UGraph G, final int k,
+            final List<Set<Node>> P) {
 
-    // Si la estimaciÃ³n informa que en el grafo G no hay
-    // un clique de tamaÃ±o mayor que LB, se corta esta rama
-    // de la exploraciÃ³n recursiva.
-    if (LB >= UB) { return new HashSet<>(); }
+        // Sirve para mantener relaciÃ³n entre nodo y variable asociada.
+        final Map<Node, BoolVar> nodeToBoolVar = new IdentityHashMap<>();
 
-    final Node v = G.getMaxDegreeNode();
-    final UGraph Gv = G.subgraphInducedBy(v);
-    final Set<Node> _C = new HashSet<>(C); //vecinos de V sin V creo que es esto 
-    _C.add(v);
-    
-    final Set<Node> C1 = _solve(Gv, _C, LB);
-    if (C1.size() > LB)
-      LB = C1.size();
-        
-    final UGraph Gm = G.subgraphInducedMinus(v, k);
-    final Set<Node> C2 = _solve(Gm, C, LB);
+        // Se crea el modelo con las clÃ¡usulas duras
+        // (clÃ¡usulas de satisfacciÃ³n obligatoria).
+        final CpModel model = getHardModel(G, k, nodeToBoolVar);
 
-    return (C1.size() > C2.size()) ? C1 : C2;
-    
-  }
-  
-  private int overstimation (final UGraph G, final int k) {                           
-    
-    // Se obtiene una particiÃ³n del grafo.
-    final List<Set<Node>> P = UGraphUtilities.partition(G); //implementar en este metodo estatico el algoritmo voraz
-    //P es una particion
-    
-    // Sil particiÃ³n no tiene suficientes subconjuntos, no se resuelve.
-    if (P.size() < k) {
-      return 0;
+        // Se aÃ±aden las clÃ¡usulas blandas (de satisfacciÃ³n a maximizar).
+        final BoolVar[] equivalents = addSoftClauses(model, P, nodeToBoolVar);
+
+        // Se instancia un resolvedor para problemas de programaciÃ³n con restricciones.
+        final CpSolver solver = new CpSolver();
+        // Se resuelve el modelo.
+        final CpSolverStatus resultStatus = solver.solve(model);
+
+        // Se chequea el tipo resultado obtenido.
+        if ((resultStatus == CpSolverStatus.OPTIMAL)
+                || (resultStatus == CpSolverStatus.FEASIBLE)) {
+            final int satisfied = countSatisfiedSoftClauses(solver, equivalents);
+            return satisfied;
+        }
+        else if (resultStatus == CpSolverStatus.INFEASIBLE) {
+            return 0;
+        }
+        else // Modelo mal formado o problemas en el resolvedor
+        {
+            throw new IllegalArgumentException("Error en formulaciÃ³n del modelo");
+        }
+
     }
-    
-    return solveMaxSATModel(G, k, P); //intentamos mejorar la sobreestimacion
-    
-  }
-  
-  private int solveMaxSATModel (final UGraph G, final int k,
-                                final List<Set<Node>> P) {
-    
-    // Sirve para mantener relaciÃ³n entre nodo y variable asociada.
-    final Map<Node, BoolVar> nodeToBoolVar = new IdentityHashMap<>();    
-    
-    // Se crea el modelo con las clÃ¡usulas duras
-    // (clÃ¡usulas de satisfacciÃ³n obligatoria).
-    final CpModel model = getHardModel(G, k, nodeToBoolVar);
-    
-    // Se aÃ±aden las clÃ¡usulas blandas (de satisfacciÃ³n a maximizar).
-    final BoolVar[] equivalents = addSoftClauses(model, P, nodeToBoolVar);
-    
-    // Se instancia un resolvedor para problemas de programaciÃ³n con restricciones.
-    final CpSolver solver = new CpSolver();
-    // Se resuelve el modelo.
-    final CpSolverStatus resultStatus = solver.solve(model);
 
-    // Se chequea el tipo resultado obtenido.
-    if ((resultStatus == CpSolverStatus.OPTIMAL) ||
-        (resultStatus == CpSolverStatus.FEASIBLE)) {
-      final int satisfied = countSatisfiedSoftClauses(solver, equivalents);
-      return satisfied;
-    } else if (resultStatus == CpSolverStatus.INFEASIBLE)
-      return 0;
-    else // Modelo mal formado o problemas en el resolvedor
-      throw new IllegalArgumentException("Error en formulaciÃ³n del modelo");
+    private CpModel getHardModel(final UGraph G, final int k,
+            final Map<Node, BoolVar> nodeToBoolVar) {
 
-  }
+        // Modelo para la instancia del problema MaxSAT
+        final CpModel model = new CpModel();
 
-  private CpModel getHardModel (final UGraph G, final int k,
-                                final Map<Node, BoolVar> nodeToBoolVar) {
-    
-    // Modelo para la instancia del problema MaxSAT
-    final CpModel model = new CpModel();
-    
-    // ClaÃºsulas booleanas del modelo
-    
-    return model;
-    
-  } 
-    
-  private BoolVar[] addSoftClauses (final CpModel model,
-                                    final List<Set<Node>> P,
-                                    final Map<Node, BoolVar> nodeToBoolVar) {
-    
-    // ObtenciÃ³n de clÃ¡usulas blandas
-    final Set<Literal[]> softClauses = getSoftClauses(P, nodeToBoolVar);
-    
-    // IncorporaciÃ³n de restricciones de clÃ¡usulas blandas
-    // Por cada clÃ¡usula blanda, se introduce una variable equivalente.
-    final BoolVar[] s = new BoolVar[softClauses.size()];
-    int j = 0;
-    for (final Literal[] clause: softClauses) {
-      
-      s[j] = OR(model, clause);
-      ++j;
-      
+        // ClaÃºsulas booleanas del modelo
+        return model;
+
     }
-    
-    // ExpresiÃ³n lineal formada por variables booleanas; cada variable booleana
-    // se considera como una variable entera que puede tomar valores 0 Ã³ 1. Se
-    // trata de maximizar la suma de los valores de las variables equivalentes
-    // a las clÃ¡usulas blandas.
-    model.maximize(LinearExpr.sum(s));
-    
-    // Devuelve el conjunto de variables equivalentes
-    return s;
 
-  }  
-  
-  private Set<Literal[]> getSoftClauses (final List<Set<Node>> P,
-                                         final Map<Node, BoolVar> nodeToBoolVar) {
-    
-    final Set<Literal[]> softClauses = new LinkedHashSet<>();
-    
-    final ArrayList<Literal> literals = new ArrayList<>();
-    for (final Set<Node> part: P) {
-      for (final Node v: part)
-        if (nodeToBoolVar.containsKey(v))
-          literals.add(nodeToBoolVar.get(v));
-      softClauses.add(literals.toArray(Literal[]::new));
-      literals.clear();
+    private BoolVar[] addSoftClauses(final CpModel model,
+            final List<Set<Node>> P,
+            final Map<Node, BoolVar> nodeToBoolVar) {
+
+        // ObtenciÃ³n de clÃ¡usulas blandas
+        final Set<Literal[]> softClauses = getSoftClauses(P, nodeToBoolVar);
+
+        // IncorporaciÃ³n de restricciones de clÃ¡usulas blandas
+        // Por cada clÃ¡usula blanda, se introduce una variable equivalente.
+        final BoolVar[] s = new BoolVar[softClauses.size()];
+        int j = 0;
+        for (final Literal[] clause : softClauses) {
+
+            s[j] = OR(model, clause);
+            ++j;
+
+        }
+
+        // ExpresiÃ³n lineal formada por variables booleanas; cada variable booleana
+        // se considera como una variable entera que puede tomar valores 0 Ã³ 1. Se
+        // trata de maximizar la suma de los valores de las variables equivalentes
+        // a las clÃ¡usulas blandas.
+        model.maximize(LinearExpr.sum(s));
+
+        // Devuelve el conjunto de variables equivalentes
+        return s;
+
     }
-    
-    return softClauses;
-    
-  }  
-  
-  private BoolVar OR (final CpModel model, final Literal[] literals) { 
-    
-    // TODO: variable equivalente a la clÃ¡usula formada por literales.
 
-  }
+    private Set<Literal[]> getSoftClauses(final List<Set<Node>> P,
+            final Map<Node, BoolVar> nodeToBoolVar) {
 
-  private int countSatisfiedSoftClauses (final CpSolver solver,
-                                         final BoolVar[] equivalents) { //las blandas no las implementamos al modelo, solo las manejamos 
-    
-    int satisfied = 0;
-    for (final BoolVar x: equivalents) {
-      if (solver.booleanValue(x))
-        ++satisfied;
+        final Set<Literal[]> softClauses = new LinkedHashSet<>();
+
+        final ArrayList<Literal> literals = new ArrayList<>();
+        for (final Set<Node> part : P) {
+            for (final Node v : part) {
+                if (nodeToBoolVar.containsKey(v)) {
+                    literals.add(nodeToBoolVar.get(v));
+                }
+            }
+            softClauses.add(literals.toArray(Literal[]::new));
+            literals.clear();
+        }
+
+        return softClauses;
+
     }
-    
-    return satisfied;
-    
-  }
+
+    private BoolVar OR(final CpModel model, final Literal[] literals) {
+        final BoolVar or = model.newBoolVar("or");
+        for (Literal lit : literals) {
+            model.addImplication(lit, or);
+        }
+        model.addBoolOr(literals).onlyEnforceIf(or);
+
+        return or;
+
+    }
+
+    private int countSatisfiedSoftClauses(final CpSolver solver,
+            final BoolVar[] equivalents) { //las blandas no las implementamos al modelo, solo las manejamos 
+
+        int satisfied = 0;
+        for (final BoolVar x : equivalents) {
+            if (solver.booleanValue(x)) {
+                ++satisfied;
+            }
+        }
+
+        return satisfied;
+
+    }
 
 }
--- src/maxclq/SCE.java
+++ src/maxclq/SCE.java
@@ -1,4 +1,5 @@
 package maxclq;
+
 /**
  *
  * @author MAZ
@@ -8,56 +9,190 @@ import com.google.ortools.sat.Literal;
 import java.util.ArrayList;
 import java.util.List;
 //
-final class SCE {
-  
-  static void atLeast (final int k,
-                       final List<Literal> literals,
-                       final CpModel model) {
 
-    final int n = literals.size();
-    
-    //
-    // TODO
-    //
-    
-  }  
-  
-  static void atMost (final int k,
-                      final List<Literal> literals,
-                      final CpModel model) {
-    
-    final int n = literals.size();
-    
-    //
-    // TODO
-    //
-    
-  }
-  
-  static void _atMost (final int k,
-                       final List<Literal> literals,
-                       final CpModel model) {
-    
-    final int n = literals.size();
-    final BoolVarMatrix R = new BoolVarMatrix(n - 1, k, model);
-    
-    //
-    // TODO
-    //
-    
-  }
-  
-  static void _atLeast (final int k,
-                        final List<Literal> literals,
-                        final CpModel model) {
-    
-    final int n = literals.size();
-    final BoolVarMatrix R = new BoolVarMatrix(n, k, model);
-    
-    //
-    // TODO
-    //
-    
-  }
-  
+final class SCE {
+
+    static void atLeast(final int k,
+            final List<Literal> literals,
+            final CpModel model) {
+
+        final int n = literals.size();
+
+        if (k > 0) { // k == 0 es de cumplimiento trivial
+
+            if ((k > n) || (n == 0)) {
+                throw new IllegalArgumentException("Restricción insatisfacible");
+            }
+
+            if (n > 0) {
+
+                if (k == 1) {
+
+                    model.addBoolOr(literals);
+
+                }
+                else if ((k > 1) && (k < n)) {
+                    // Al menos k ovejas blancas == a lo sumo n - k ovejas negras
+                    if ((n * k) <= ((n - 1) * (n - k))) {
+                        _atLeast(k, literals, model);
+                    }
+                    else {
+                        List<Literal> negated = new ArrayList<>();
+                        for (Literal l : literals) {
+                            negated.add(l.not());
+                        }
+                        _atMost(n - k, negated, model);
+                    }
+                }
+                else if (k == n) {
+                    // Al menos n de n literales es exigir que se cumplan todos.
+                    model.addBoolAnd(literals);
+                }//mejor hacerlo con un bucle sobre literles en lugar de sobre i
+            }
+
+        }
+
+    }
+
+    static void atMost(final int k,
+            final List<Literal> literals,
+            final CpModel model) {
+
+        final int n = literals.size();
+
+        if (n > 0) { // A lo sumo k de 0 literales es de cumplimiento trivial
+
+            if (k == 0) {
+
+                // A lo sumo 0 ovejas blancas == al menos n ovejas negras
+                for (Literal x : literals) {
+                    model.addEquality(x, 0);
+                }
+            }
+            else if (k == 1) {
+
+                atMost1(literals, phi);
+
+            }
+            else if ((k > 1) && (k < (n - 1))) {
+                if (((n - 1) * k) < (n * (n - k))) {
+                    _atMost(k, literals, phi);
+                }
+                else {
+                    List<BooleanLiteral> negated = new ArrayList<>();
+                    for (BooleanLiteral l : literals) {
+                        negated.add(l.not());
+                    }
+                    _atLeast(n - k, negated, phi);
+                }
+            }
+            else if (k == (n - 1)) {
+
+                // A lo sumo n - 1 ovejas blancas == al menos 1 oveja negra
+                List<BooleanLiteral> negated = new ArrayList<>();
+                for (BooleanLiteral l : literals) {
+                    negated.add(l.not());
+                }
+                phi.addClause(new DisjunctiveBooleanClause(negated));
+            }
+            else if (k >= n) {
+
+                // Cuando k >= n, se cumple trivialmente a lo sumo k de un total de n.
+                // Se queda vacío.
+            }
+
+        }
+
+    }
+
+    static void _atMost(final int k,
+            final List<Literal> literals,
+            final CpModel model) {
+
+        final int n = literals.size();
+        final BoolVarMatrix R = new BoolVarMatrix(n - 1, k, model);
+        // Codificación del artículo
+        //1
+        for (int i = 1; i < n; i++) {
+            Literal litMat = R.getPositiveLiteral(i, k);
+            Literal x = literals.get(i - 1); //el getPositive... ya te hace el i-1, el get es un get normal
+            //como la R empieza en 1 hay que restarle 1, y el get normal no lo hace
+            model.addBoolOr(new Literal[]{x.not(), litMat});
+        }
+
+        //2 
+        for (int j = 2; j <= k; j++) {
+            Literal litMat = R.getNegativeLiteral(1, j);
+
+            model.addBoolOr(new Literal[]{litMat});
+
+        }
+
+        //3 
+        for (int i = 2; i <= n - 1; i++) {
+            for (int j = 1; j <= k; j++) {
+                Literal litMat1 = R.getNegativeLiteral(i - 1, j);
+                Literal litMat2 = R.getPositiveLiteral(i, j);
+                model.addBoolOr(new Literal[]{ litMat1, litMat2 });
+            }
+        }
+        //4 
+        for (int i = 2; i <= n - 1; i++) {
+            Literal l = literals.get(i - 1);//-1 por lo de antes, el get no resta el getNeg si 
+            //como solo cambia en cada i, puedes sacarlo al primer bucle
+            for (int j = 2; j <= k; j++) {
+                Literal litMat1 = R.getNegativeLiteral(i - 1, j - 1);
+                Literal litMat2 = R.getPositiveLiteral(i, j);
+                List<Literal> listaAux = new ArrayList<>();
+                model.addBoolOr(new Literal[]{ l.not(), litMat1, litMat2 });
+            }
+        }
+        //5
+        for (int i = 2; i <= n; i++) {
+            Literal l = literals.get(i - 1);//-1 por lo de antes, el get no resta el getNeg si 
+            Literal litMat1 = R.getNegativeLiteral(i - 1, k);
+            model.addBoolOr(new Literal[]{ l.not(), litMat1 });
+        }
+
+    }
+
+static void _atLeast(final int k,
+            final List<Literal> literals,
+            final CpModel model) {
+
+        final int n = literals.size();
+        final BoolVarMatrix R = new BoolVarMatrix(n, k, model);
+        for (int j = 1; j <= k; j++) {
+            Literal litMat = R.getPositiveLiteral(n, j);
+        }
+        model.addBoolAnd(litMat);
+
+        for (int i = 2; i <= n; i++) {
+            Literal l = literals.get(i - 1);
+            for (int j = 2; j <= k; j++) {
+                BooleanLiteral litMat1 = R.getNegativeLiteral(i, j);
+                BooleanLiteral litMat2 = R.getPositiveLiteral(i - 1, j);
+                BooleanLiteral litMat3 = R.getPositiveLiteral(i - 1, j - 1);
+
+                phi.addClause(new DisjunctiveBooleanClause(litMat1, litMat2, l));
+                phi.addClause(new DisjunctiveBooleanClause(litMat1, litMat2, litMat3));
+
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            BooleanLiteral l = literals.get(i - 1);
+            BooleanLiteral litMat1 = R.getNegativeLiteral(i, 1);
+            BooleanLiteral litMat2 = R.getPositiveLiteral(i - 1, 1);
+            phi.addClause(new DisjunctiveBooleanClause(litMat1, litMat2, l));
+        }
+        phi.addClause(new DisjunctiveBooleanClause(R.getNegativeLiteral(1, 1), literals.get(0)));
+        for (int j = 2; j <= k; j++) {
+            phi.addClause(new DisjunctiveBooleanClause(R.getNegativeLiteral(1, j)));
+        }
+        for (int i = 1; i < k; i++) {
+            phi.addClause(new DisjunctiveBooleanClause(R.getNegativeLiteral(i, k)));
+        }
+
+    }
+
 }
