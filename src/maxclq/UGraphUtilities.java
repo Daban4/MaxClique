356d9b26b8d92a1703ae50e39e1b764c7bd74af1
nataliacjmm <149517470+nataliacjmm@users.noreply.github.com>
2026-01-02 17:05:10 +0100

    Add files via upload
    TODO hecho

--- src/maxclq/UGraphUtilities.java
+++ src/maxclq/UGraphUtilities.java
@@ -20,7 +20,35 @@ public class UGraphUtilities {
     
     final List<Set<Node>> P = new LinkedList<>();
 
-    // TODO
+    final int numNodes = G.nodes();
+    for (int i = 1; i <= numNodes; ++i) {
+      final Node v = G.node(i);
+
+      boolean foundSet = false;
+      for (final Set<Node> S : P) {
+        
+        boolean isCompatible = true; 
+        
+        for (final Node u : S) {
+          if (G.neighbours(v, u)) { 
+            isCompatible = false;
+            break; 
+          }
+        }
+        
+        if (isCompatible) {
+          S.add(v);
+          foundSet = true;
+          break; 
+        }
+      }
+      
+      if (!foundSet) {
+        final Set<Node> newSet = new LinkedHashSet<>();
+        newSet.add(v);
+        P.add(newSet);
+      }
+    }
     
     return P;
     
